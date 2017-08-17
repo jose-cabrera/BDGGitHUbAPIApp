6673e6c116c7ebb76a15b943828ec8caf1b6cb93
Jose Cabrera <deltadax18@gmail.com>
2017-08-16 20:48:43 -0600

    iconos

---
+++ app/src/main/java/com/deltadax/bdggithubapiapp/activity/DetailActivity.java
@@ -0,0 +1,15 @@
+package com.deltadax.bdggithubapiapp.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.deltadax.bdggithubapiapp.R;
+
+public class DetailActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detail);
+    }
+}
