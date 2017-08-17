588200982f0c858bf59f3b3930fc726ae450f893
Jose Cabrera <deltadax18@gmail.com>
2017-08-16 18:00:04 -0600

    Base commit

---
+++ app/src/main/java/com/deltadax/bdggithubapiapp/App.java
@@ -0,0 +1,16 @@
+package com.deltadax.bdggithubapiapp;
+
+import com.orm.SugarApp;
+
+/**
+ * Created by JosePablo on 16/08/17.
+ */
+
+public class App extends SugarApp {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+    }
+}
--- app/src/main/java/com/deltadax/bdggithubapiapp/activity/MainActivity.java
+++ app/src/main/java/com/deltadax/bdggithubapiapp/activity/MainActivity.java
@@ -1,4 +1,4 @@
-package com.deltadax.bdggithubapiapp;
+package com.deltadax.bdggithubapiapp.activity;
 
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
@@ -8,6 +8,8 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+
+import com.deltadax.bdggithubapiapp.R;
 
 public class MainActivity extends AppCompatActivity {
 
